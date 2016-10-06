d0e33455b031fab2eb5199e706ac24ae9b018c05
DianeRay <ruikang.dai@gmail.com>
2016-10-06 00:44:26 -0400

    Update the simple test case for todo_list
    I am considering how to improve this part of my design. This is the
    part I want to change to dynamic an online judge.

--- lib/todo_list.py
+++ lib/todo_list.py
@@ -1,10 +1,18 @@
 # 1.Remove completed jobs from queue
 # 2.Add existing jobs to queue
+from lib import py
 class remain(object):
     def __init__(self, queue, concur):
         # type: (object, object) -> object
         """
-
         :rtype: object
         """
-        pass
+        unfinished = self.find_jobs()
+        return unfinished
+
+    def find_jobs(self):
+        job1 = py.job_py(proj_path='print "j1"')
+        job2 = py.job_py(proj_path='print "j2"')
+        job3 = py.job_py(proj_path='print "j3"')
+        job4 = py.job_py(proj_path='print "j4"')
+        return [job1, job2, job3, job4]
